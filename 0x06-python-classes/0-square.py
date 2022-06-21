8465c34c0949c0ee9576e607f51047bab942ec29
Decypher1 <mumekwe2@gmail.com>
2022-06-21 02:17:50 -0700

    Readme file and empty class square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+able File  10 lines (7 sloc)  132 Bytes
+
+#!/usr/bin/python3
+"""Documentation Square class"""
+
+
+class Square():
+    """Square Class
+    Attributes:
+        None"""
+
+    pass
