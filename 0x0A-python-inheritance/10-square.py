b8cf100bfa7395e498d61a9277baebe898722df2
Decypher1 <mumekwe2@gmail.com>
2022-07-04 12:33:52 -0700

    squaree

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,71 +1,20 @@
 #!/usr/bin/python3
-"""Documentation for BaseGeometry class"""
-
-
-class BaseGeometry:
-
-    """Base Geometry class """
-
-    def area(self):
-        """Area function """
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """integer_validator function
-        validates the value
-        """
-
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-
-    """Class Rectangle"""
-
-    def __init__(self, width, height):
-
-        """Instantiation with width and height"""
-
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Area function
-        Returns: The area of the rectangle
-        """
-
-        return self.__width * self.__height
-
-    def __str__(self):
-        """The function for use in print() and str()
-        """
-
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+"""This module creates a class for task 10"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-
-    """Class Square"""
-
+    """
+        Class Square extends from  Rectangle
+    """
     def __init__(self, size):
-
-        """Instantiation with size"""
-
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        super().integer_validator("size", size)
         self.__size = size
+        # super().__init__(size, size)
 
-        def area(self):
-            """Area function for the square object
-            Returns:
-            Area of the square
-        """
+    def area(self):
+        return self.__size ** 2
 
-            return self.__size ** 2
+    def __str__(self):
+        return "[{}] {}/{}".format(self.__class__.__name__,
+                                   self.__size, self.__size)
